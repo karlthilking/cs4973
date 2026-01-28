933f1d32e2a17d5a16590c496a1772221461f9ea
karlthilking <thilking.k@northeastern.edu>
2026-01-28 01:22:29 -0500

    updated Makefile and matrix.c

--- hw2/matrix.c
+++ hw2/matrix.c
@@ -5,15 +5,17 @@
 #include <string.h>
 #include <time.h>
 
+static int w = 0, h = 0;
+
 void
-display_matrix(int matrix[5][5])
+display_matrix(int matrix[w][h])
 {
     puts("Matrix:");
-    for (int i = 0; i < 5; ++i) {
-        printf("%-4d%-4d%-4d%-4d%d\n",
-               matrix[i][0], matrix[i][1],
-               matrix[i][2], matrix[i][3],
-               matrix[i][4]);
+    for (int i = 0; i < h; ++i) {
+        for (int j = 0; j < w; ++j) {
+            printf("%-5d", matrix[i][j]);
+        }
+        printf("\n\n");
     }
 }
 
@@ -21,14 +23,15 @@ int
 main(int argc, char* argv[])
 {
     if (argc < 2) {
-        puts("Usage: ./matrix <microsecond interval>");
+        puts("Usage: ./matrix <interval length (us)> <matrix width/height>");
         exit(EXIT_FAILURE);
     }
     int interval = atoi(argv[1]);
-    int matrix[5][5];
+    w = h = atoi(argv[2]);
+    int matrix[w][h];
     memset(&matrix, 0, sizeof(matrix));
-    for (int i = 0; i < 5; i = ++i % 5) {
-        for (int j = 0; j < 5; ++j) {
+    for (int i = 0; i < h; i = ++i % h) {
+        for (int j = 0; j < w; ++j) {
             system("clear");
             ++matrix[i][j];
             display_matrix(matrix);

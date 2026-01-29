76b751d5ce7282f33d9bb220db0fb01139288e0a
karlthilking <thilking.k@northeastern.edu>
2026-01-29 11:58:17 -0500

    added preprocessing to readckpt to handle aarch64 architecture

--- hw2/readckpt.c
+++ hw2/readckpt.c
@@ -24,16 +24,23 @@ typedef struct {
     int data_size;
 } ckpt_header_t;
 
-// ordering of general registers in the 'gregset_t' array
-// in the struct mcontext_t which is contained in the struct
-// ucontext_t (as uc_mcontext) from glibc in
-// sysdeps/x86_64/sys/ucontext.h; NGREG is the number of 
-// general registers in the 'gregset_t' array
-static char *regs[NGREG] = {
+#ifdef __x86_64
+#   define NUM_REGS     NGREG
+#   define CPU          "x86_64"
+static char *regs[NUM_REGS] = {
     "R8", "R9", "R10", "R11", "R12", "R13", "R14", "R15",
     "RDI", "RSI", "RBP", "RBX", "RDX", "RAX", "RCX", "RSP",
     "RIP", "EFL", "CSGSFS", "ERR", "TRAPNO", "OLDMASK", "CR2"
 };
+#elif defined(__aarch64__)
+#   define NUM_REGS     31
+#   define CPU          "aarch64"
+static char *regs[NUM_REGS] = {
+    "X0", "X1", "X2", "X3", "X4", "X5", "X6", "X7", "X8", "X9", "X10",
+    "X11", "X12", "X13", "X14", "X15", "X16", "X17", "X18", "X19", "X20",
+    "X21", "X22", "X23", "X24", "X25", "X26", "X27", "X28", "FP", "LR"
+};
+#endif
 
 int
 read_ckpt(int ckpt_fd, ckpt_header_t ckpt_headers[], ucontext_t *ucp)
@@ -72,6 +79,14 @@ read_ckpt(int ckpt_fd, ckpt_header_t ckpt_headers[], ucontext_t *ucp)
 }
 
 void
+print_sys_info()
+{
+    printf("CPU: %s\n", CPU);
+    printf("Number of Registers: %d\n", NUM_REGS);
+    printf("sizeof ucontext_t: %zu\n", sizeof(ucontext_t));
+}
+
+void
 print_ckpt_headers(ckpt_header_t ckpt_headers[])
 {
     for (int i = 0; ; ++i) {
@@ -90,8 +105,21 @@ print_ckpt_headers(ckpt_header_t ckpt_headers[])
 void
 print_ucontext_regs(ucontext_t *ucp) 
 {
-    for (int i = 0; i < NGREG; ++i)
-        printf("%-12s%p\n", regs[i], ucp->uc_mcontext.gregs[i]);
+#ifdef __x86_64
+    for (int i = 0; i < NUM_REGS; ++i) {
+        if ((void *)ucp->uc_mcontext.gregs[i] == NULL)
+            continue;
+        printf("%s: %p\n", regs[i], ucp->uc_mcontext.gregs[i]);
+    }
+#elif define(__aarch64__)
+    printf("$sp: %p\n", ucp->uc_mcontext.sp);
+    printf("$pc: %p\n", ucp->uc_mcontext.pc);
+    for (int i = 0; i < NUM_REGS; ++i) {
+        if ((void *)ucp->uc_mcontext.regs[i] == NULL)
+            continue;
+        printf("%s: %p\n", regs[i], (void *)ucp->uc_mcontext.regs[i]);
+    }
+#endif
 }
 
 int
@@ -111,6 +139,7 @@ main(int argc, char *argv[])
     }
     if (read_ckpt(ckpt_fd, ckpt_headers, &uc) < 0)
         exit(EXIT_FAILURE);
+    print_sys_info();
     print_ckpt_headers(ckpt_headers);
     print_ucontext_regs(&uc);
     close(ckpt_fd);

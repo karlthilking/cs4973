e220e43af95b39a8d52ea1019e25bb37623f1778
karlthilking <thilking.k@northeastern.edu>
2026-02-07 21:30:45 -0500

    collatz

--- hw4/collatz.py
+++ hw4/collatz.py
@@ -2,14 +2,17 @@ def collatz(n):
     return f(n, 1)
 
 def f(n, x):
-    if n == 1:
-        return x
-    elif n % 2 == 0:
-        return f(n // 2, x + 1)
-    elif n % 2 == 1:
-        return f(3 * n + 1, x + 1)
+    while True:
+        if n == 1:
+            break
+        elif n % 2 == 0:
+            n //= 2; x += 1
+        else:
+            n = 3 * n + 1; x += 1
+    return x
 
-for i in range(1, 11):
-    print(f'collatz({i}): {collatz(i)}')
-
+for i in range(1, 100):
+    if collatz(i) < 100:
+        continue
+    print(f'collatz({i}): {collatz(i)} > 100')
 

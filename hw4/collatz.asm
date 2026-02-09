26ecad461288361b5eedbaa1f7de461e7b8800ad
karlthilking <thilking.k@northeastern.edu>
2026-02-08 19:37:13 -0500

    added comments to collatz assembly implementation

--- hw4/collatz.asm
+++ hw4/collatz.asm
@@ -7,21 +7,23 @@ newline: 	.asciz "\n"
 
 .text
 main:
-	addi 	sp, sp, -8
-	sw	s0, 0(sp)
+	addi 	sp, sp, -8      # allocate space for two ints
+	sw	s0, 0(sp)       
 	sw	s1, 4(sp)
-	li	s0, 1
-	li	s1, 100
-	
+	li	s0, 1           # loop variable i=1
+	li	s1, 100         # loop condition i<100
+
+# for (int i=1; i<100; i++)
+#       printf("collatz(%d): %d\n", i, collatz(i));
 loop:
 	li	a7, 4
 	la	a0, out1
-	ecall
+	ecall                   
 	li	a7, 1
-	mv	a0, s0
+	mv	a0, s0          
 	ecall
 	li	a7, 4
-	la	a0, out2
+	la	a0, out2         
 	ecall
 	mv	a0, s0
 	call	collatz
@@ -30,42 +32,49 @@ loop:
 	li	a7, 4
 	la 	a0, newline
 	ecall
-	addi	s0, s0, 1
-	blt	s0, s1, loop
-	
+	addi	s0, s0, 1       # i++
+	blt	s0, s1, loop    # if i < 100 continue
+
+# exit(0);
 exit:
 	li	a7, 10
 	ecall	
-	
-collatz:
-	addi	sp, sp, -4
-	sw	ra, 0(sp)
-	li	a1, 1
-	call	f
-	lw	ra, 0(sp)
-	addi	sp, sp, 4
-	jr	ra
 
+# collatz(n):
+#       return f(n,1)
+collatz:
+	addi	sp, sp, -4              # allocate space for ra
+	sw	ra, 0(sp)               # store ra
+	li	a1, 1                   
+	call	f
+	lw	ra, 0(sp)               # restore ra
+	addi	sp, sp, 4               # deallocate stack
+	ret                             # return
+
+# f(n,x):
+#       if n == 1:      return x
+#       if n % 2 == 0:  return f(n/2,x+1)
+#       if n % 2 == 1:  return f(3*n+1,x+1)
 f:
 	li	t0, 1			
 	beq	a0, t0, fone		# if x == 1
 	addi	t0, t0, 1
 	rem	t1, a0, t0
-	beqz	t1, feven		# else if x % 2 == 0
-	bnez	t1, fodd		# else if x % 2 == 1
+	beqz	t1, feven		# if x % 2 == 0
+	bnez	t1, fodd		# if x % 2 == 1
 
-# if (x==1) { return x; }
+# if n == 1: return x
 fone:
 	mv	a0, a1
 	ret
 
-# else if (x%2==0) { return f(n/2,x+1); }
+# if n % 2 == 0: return f(n/2,x+1)
 feven:
 	srli	a0, a0, 1			
 	addi	a1, a1, 1
 	j 	f
 
-# else if (x%2==1) { return f(3*n+1,x+1); }
+# if n % 2 == 1: return f(3*n+1,x+1)
 fodd:
 	li	t0, 3
 	mul	a0, a0, t0

02a2e46cabb43dafc927ce8573972d3641e32884
karlthilking <thilking.k@northeastern.edu>
2026-02-09 11:35:18 -0500

    iterative collatz

--- hw4/collatz.asm
+++ hw4/collatz.asm
@@ -4,6 +4,8 @@
 out1:       .asciz "collatz("
 out2:       .asciz "): "
 newline:    .asciz "\n"
+out3:       .asciz ", collatziter("
+
 
 .text
 main:
@@ -30,8 +32,21 @@ loop:
     call    collatz
     ecall                   # print %d, collatz(i)
     li      a7, 4
+    la      a0, out3 
+    ecall
+    li      a7, 1
+    mv      a0, s0
+    ecall
+    li      a7, 4
+    la      a0, out2
+    ecall
+    li      a7, 1
+    mv      a0, s0
+    call    collatziter
+    ecall
+    li      a7, 4
     la      a0, newline
-    ecall                   # print "\n"
+    ecall
     addi    s0, s0, 1       # i++
     blt     s0, s1, loop    # if i<n(100): continue
 
@@ -93,5 +108,45 @@ fodd:
     addi    sp, sp, 4
     ret                     # return f(3*n+1,x+1)
 
+# iterative collatz function
+# collatziter(n):
+#   for(x=0;; x++)
+#       if n==1:    break
+#       if n%2==0:  n=n/2
+#       if n%2==1:  n=3*n+1
+#   return x
+collatziter:
+    addi    sp, sp, -12
+    sw      s0, 0(sp)
+    sw      s1, 4(sp)
+    sw      s2, 8(sp)
+    li      s0, 1           # x (number of iterations)
+    li      s1, 1           
+    li      s2, 2
 
+collatzloop:
+    beq     a0, s1, collatzret
+    rem     t0, a0, s2      # n%2
+    beqz    t0, collatzeven
+    bnez    t0, collatzodd
+
+collatzeven:
+    srli    a0, a0, 1       # n=n/2
+    addi    s0, s0, 1       # x=x+1
+    j       collatzloop
+
+collatzodd:
+    li      t0, 3
+    mul     a0, t0, a0      # n=3*n
+    addi    a0, a0, 1       # n=n+1
+    addi    s0, s0, 1       # x=x+1
+    j       collatzloop
+
+collatzret:
+    mv      a0, s0
+    lw      s0, 0(sp)
+    lw      s1, 4(sp)
+    lw      s2, 8(sp)
+    addi    sp, sp, 12
+    ret
 

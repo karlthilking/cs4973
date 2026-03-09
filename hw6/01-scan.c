b2dec95ea04cc83f7a275c3c0fcb264eab96bc27
Karl Thilking <thilking.k@northeastern.edu>
2026-03-08 22:19:54 -0400

    updated scan test for lru edge case

--- hw6/01-scan.c
+++ hw6/01-scan.c
@@ -4,7 +4,7 @@
 #define CACHE_SIZE 4096
 #define BLOCK_SIZE 128
 
-static int A[CACHE_SIZE + BLOCK_SIZE];
+static int A[(CACHE_SIZE + BLOCK_SIZE) / sizeof(int)];
 
 int
 main(int argc, char *argv[])
@@ -14,15 +14,14 @@ main(int argc, char *argv[])
      *  Allocate array which is the size of the cpu cache plus one cache line
      *  and scan through array in order to invoke lru corner case behavior
      */
-    const int n_epochs      = (argc > 1) ? atoi(argv[1]) : 100;
+    const int n_epochs      = argc > 1 ? atoi(argv[1]) : 10;
     const int n_cache_lines = CACHE_SIZE / BLOCK_SIZE;
-    
-    const int n_iterations  = CACHE_SIZE / sizeof(int);
+    const int n_iterations  = (CACHE_SIZE + BLOCK_SIZE) / sizeof(int);
     const int step          = BLOCK_SIZE / sizeof(int);
     
     for (int epoch = 0; epoch < n_epochs; ++epoch)
         for (int i = 0; i < n_iterations; i += step)
-            A[i]++;
+            A[i] = 0;
 
     exit(0);
 }
